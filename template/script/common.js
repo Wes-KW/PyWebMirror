f8eae91b3234aa7b77cde058380307b25aad8344
Wes-KW <dotdotdashdash2024@hotmail.com>
2024-10-19 17:48:30 +0000

    Refactor All Code (7)

--- template/script/common.js
+++ template/script/common.js
@@ -17,47 +17,6 @@ var HOST = $HOST;
 var CMETHOD = $CMETHOD;
 var ALLOWRULES = $ALLOWRULES;
 var DENYRULES = $DENYRULES;
-var TOKEN = getToken();
-
-
-var getCookie = function getCookie() {
-	var split = function split(separator, count) {
-		var list = String.prototype.split.call(this, separator);
-	
-		if (!count || count > list.length) {
-			count = list.length;
-		}
-	
-		var nList = [];
-		var extra = [];
-		for (var i = 0; i < list.length; i++) {
-			if (nList.length < count - 1) {
-				nList.push(list[i]);
-			} else {
-				extra.push(list[i]);
-			}
-		}
-	
-		nList.push(extra.join(separator));
-		return nList;
-	}
-
-	var cookieList = document.cookie.split("; ");
-	var cookieSet = {};
-	for (var i = 0; i < cookieList.length; i++) {
-		var keyValuePair = split.call(cookieList[i], "=", 2);
-		if (keyValuePair.length != 2) {
-			continue;
-		}
-		cookieSet[keyValuePair[0]] = keyValuePair[1];
-	}
-	return cookieSet;
-}
-
-
-var getToken = function getToken() {
-	return getCookie()["__PyWebMirror_TOKEN"];
-}
 
 
 var isURIDecoded = function isURIDecoded(url) {
